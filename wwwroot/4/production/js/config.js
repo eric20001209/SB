47fd5e26ec22b0a53e1b510c63bc8b0de6a8f829
eric2000.c <eric2000.c@gmail.com>
2019-06-26 15:51:06 +1200

    ..

--- wwwroot/4/production/js/config.js
+++ wwwroot/4/production/js/config.js
@@ -1,10 +1,10 @@
 ﻿var prefix = "https://localhost:44398/api";
 
-//$(function () {
-//    $(document).ajaxStart(function () {
-//        $("body").addClass("loading");
-//    })
-//        .ajaxStop(function () {
-//            $("body").removeClass("loading");
-//        });
-//}); //loading effect。。。
+$(function () {
+    $(document).ajaxStart(function () {
+        $("body").addClass("loading");
+    })
+        .ajaxStop(function () {
+            $("body").removeClass("loading");
+        });
+}); //loading effect。。。
