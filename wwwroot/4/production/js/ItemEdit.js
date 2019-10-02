faaac75479a03ee47647dd5acd921c3a77a93ec0
eric2000.c <eric2000.c@gmail.com>
2019-10-02 15:16:26 +1300

    。。。

--- wwwroot/4/production/js/ItemEdit.js
+++ wwwroot/4/production/js/ItemEdit.js
@@ -1,284 +1,158 @@
-﻿
-(function ($) {
-
-    'use strict';
-
-    var EditableTable = {
-
-        options: {
-            addButton: '#addToTable',
-            table: '#datatable-editable',
-            dialog: {
-                wrapper: '#dialog',
-                cancelButton: '#dialogCancel',
-                confirmButton: '#dialogConfirm',
-            }
-        },
-
-        initialize: function () {
-            this
-                .setVars()
-                .build()
-                .events();
-        },
-
-        setVars: function () {
-            this.$table = $(this.options.table);
-            this.$addButton = $(this.options.addButton);
-
-            // dialog
-            this.dialog = {};
-            this.dialog.$wrapper = $(this.options.dialog.wrapper);
-            this.dialog.$cancel = $(this.options.dialog.cancelButton);
-            this.dialog.$confirm = $(this.options.dialog.confirmButton);
-
-            return this;
-        },
-
-        build: function (row) {
-            //debugger
-            var txt = '';
-            txt += '<td class="actions">';
-            txt += '<a href="#" class="hidden on-editing save-row"><i class="fa fa-save"></i></a>';
-            txt += '<a href="#" class="hidden on-editing cancel-row"><i class="fa fa-times"></i></a>';
-            txt += '<a href="#" class="on-default edit-row"><i class="fa fa-pencil"></i></a>';
-            txt += '<a href="#" class="on-default remove-row"><i class="fa fa-trash-o"></i></a></td>';
-            $(row).addClass('gradeX');
-
-            this.datatable = this.$table.DataTable({
-                //"aoColumns": [
-                //    null,
-                //    null,
-                //    null,
-                //    null,
-                //    null,
-                //    null,
-                //    null,
-                //    { "bSortable": false }
-                //]
-
-
-                "ajax": 
-                {
-                    "url": "https://localhost:44398/api/item/itemlist",
-                    "dataSrc": ""
-                },
-
-                "aoColumns": [
-                    {
-                        "render": function (code) {
-                            return '<td>'+code+'</td>'
-                        },
-                        "data": "code",
-
-                    },
-                    { "data": "name" },
-                    { "data": "name_cn" },
-                    { "data": "cat" },
-                    { "data": "unit" },
-                    { "data": "price" },
-                    { "data": "cost" },
-                    {
-                        "data": "id" ,
-                        "bSortable": false,
-                        "render": function () {
-                       ////     debugger
-                            return txt
-
-                        }
-                    }
-                ]
-
-            });
-            window.dt = this.datatable;
-
-            return this;
-        },
-
-        events: function () {
-            var _self = this;
-
-            this.$table
-                .on('click', 'a.save-row', function (e) {
-                    e.preventDefault();
-
-                    _self.rowSave($(this).closest('tr'));
-                })
-                .on('click', 'a.cancel-row', function (e) {
-                    e.preventDefault();
-
-                    _self.rowCancel($(this).closest('tr'));
-                })
-                .on('click', 'a.edit-row', function (e) {
-                    e.preventDefault();
-
-                    _self.rowEdit($(this).closest('tr'));
-                })
-                .on('click', 'a.remove-row', function (e) {
-                    e.preventDefault();
-
-                    var $row = $(this).closest('tr');
-
-                    $.magnificPopup.open({
-                        items: {
-                            src: '#dialog',
-                            type: 'inline'
-                        },
-                        preloader: false,
-                        modal: true,
-                        callbacks: {
-                            change: function () {
-                                _self.dialog.$confirm.on('click', function (e) {
-                                    e.preventDefault();
-
-                                    _self.rowRemove($row);
-                                    $.magnificPopup.close();
-                                });
-                            },
-                            close: function () {
-                                _self.dialog.$confirm.off('click');
-                            }
-                        }
-                    });
-                });
-
-            this.$addButton.on('click', function (e) {
-                e.preventDefault();
-
-                _self.rowAdd();
-            });
-
-            this.dialog.$cancel.on('click', function (e) {
-                e.preventDefault();
-                $.magnificPopup.close();
-            });
-
-            return this;
-        },
-
-        // ==========================================================================================
-        // ROW FUNCTIONS
-        // ==========================================================================================
-        rowAdd: function () {
-            this.$addButton.attr({ 'disabled': 'disabled' });
-
-            var actions,
-                data,
-                $row;
-
-            actions = [
-                '<a href="#" class="hidden on-editing save-row"><i class="fa fa-save"></i></a>',
-                '<a href="#" class="hidden on-editing cancel-row"><i class="fa fa-times"></i></a>',
-                '<a href="#" class="on-default edit-row"><i class="fa fa-pencil"></i></a>',
-                '<a href="#" class="on-default remove-row"><i class="fa fa-trash-o"></i></a>'
-            ].join(' ');
-
-            data = this.datatable.row.add(['', '', '', '', '', '', '', actions]);
-            $row = this.datatable.row(data[0]).nodes().to$();
-
-            $row
-                .addClass('adding')
-                .find('td:last')
-                .addClass('actions');
-
-            this.rowEdit($row);
-
-            this.datatable.order([0, 'asc']).draw(); // always show fields
-        },
-
-        rowCancel: function ($row) {
-            var _self = this,
-                $actions,
-                i,
-                data;
-
-            if ($row.hasClass('adding')) {
-                this.rowRemove($row);
-            } else {
-
-                data = this.datatable.row($row.get(0)).data();
-                this.datatable.row($row.get(0)).data(data);
-
-                $actions = $row.find('td.actions');
-                if ($actions.get(0)) {
-                    this.rowSetActionsDefault($row);
-                }
-
-                this.datatable.draw();
-            }
-        },
-
-        rowEdit: function ($row) {
-            var _self = this,
-                data;
-
-            data = this.datatable.row($row.get(0)).data();
-
-            $row.children('td').each(function (i) {
-                var $this = $(this);
-
-                if ($this.hasClass('actions')) {
-                    _self.rowSetActionsEditing($row);
-                } else {
-                    $this.html('<input type="text" class="form-control input-block" value="' + data[i] + '"/>');
-                }
-            });
-        },
-
-        rowSave: function ($row) {
-            alert('123');
-            var _self = this,
-                $actions,
-                values = [];
-
-            if ($row.hasClass('adding')) {
-                this.$addButton.removeAttr('disabled');
-                $row.removeClass('adding');
-            }
-
-            values = $row.find('td').map(function () {
-                var $this = $(this);
-
-                if ($this.hasClass('actions')) {
-                    _self.rowSetActionsDefault($row);
-                    return _self.datatable.cell(this).data();
-                } else {
-                    return $.trim($this.find('input').val());
-                }
-            });
-
-            this.datatable.row($row.get(0)).data(values);
-
-            $actions = $row.find('td.actions');
-            if ($actions.get(0)) {
-                this.rowSetActionsDefault($row);
-            }
-
-            this.datatable.draw();
-        },
-
-        rowRemove: function ($row) {
-            if ($row.hasClass('adding')) {
-                this.$addButton.removeAttr('disabled');
-            }
-
-            this.datatable.row($row.get(0)).remove().draw();
-        },
-
-        rowSetActionsEditing: function ($row) {
-            $row.find('.on-editing').removeClass('hidden');
-            $row.find('.on-default').addClass('hidden');
-        },
-
-        rowSetActionsDefault: function ($row) {
-            $row.find('.on-editing').addClass('hidden');
-            $row.find('.on-default').removeClass('hidden');
-        }
-
+﻿$(function () {
+    Number.prototype.formatMoney = function (places, symbol, thousand, decimal) {
+        places = !isNaN(places = Math.abs(places)) ? places : 2;
+        symbol = symbol !== undefined ? symbol : "$";
+        thousand = thousand || ",";
+        decimal = decimal || ".";
+        var number = this,
+            negative = number < 0 ? "-" : "",
+            i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
+            j = (j = i.length) > 3 ? j % 3 : 0;
+        return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
     };
+    // Default usage and custom precision/symbol :
+    var revenue = 12345678;
+    //alert(revenue.formatMoney()); // $12,345,678.00
+    //alert(revenue.formatMoney(0, "HK$ ")); // HK$ 12,345,678
+    // European formatting:
+    var price = 4999.99;
+    //alert(price.formatMoney(2, "€", ".", ",")); // €4.999,99
+    // It works for negative values, too:
+    //alert((-500000).formatMoney(0, "£ ")); // £ -500,000
+    var price = (12345.99).formatMoney(); // "$12,345.99"
+    // Remove non-numeric chars (except decimal point/minus sign):
+    //priceVal = parseFloat(price.replace(/[^0-9-.]/g, '')); // 12345.99
+});
 
-    $(function () {
-        EditableTable.initialize();
+var $table = $('#itemlist')
+var $remove = $('#remove')
+function loaddata()
+{
+    getdata();
+}
+function getdata() {
+    var uri = prefix + '/item/itemlist';
+    var tabledata;
+    $.ajax({
+        type: "get",
+        url: uri,
+        async: true,
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+            //alert(JSON.stringify(data));
+
+            tabledata = data;
+            for (var q = 0; q < tabledata.length; q++) {
+                tabledata[q].price = tabledata[q].price.formatMoney();
+                tabledata[q].cost = tabledata[q].cost.formatMoney();
+            }
+            initTable(tabledata, '#itemlist')
+        },
+        error: function (data) {
+            if (data.status == 401)
+                //alert('Token Expired!! Redirect to login page!');
+                setTimeout("window.location.href='login.html'", 3000);
+            //    $("#showloginModal").click();
+
+        }
+    });
+}
+
+function responseHandler(res) {
+    $.each(res.rows, function (i, row) {
+        row.state = $.inArray(row.id, selections) !== -1
+    })
+    return res
+}
+
+window.operateEvents = {
+    'click .edit-row': function (e, value, row, index) {
+        alert('You click like action, row: ' + JSON.stringify(row))
+    },
+    //'click .remove-row': function (e, value, row, index) {
+    //    $table.bootstrapTable('remove', {
+    //        field: 'id',
+    //        values: [row.id]
+    //    })
+    //}
+}
+
+function operateFormatter(value, row, index) {
+    return [
+        '<a href="#" class="edit-row"><i class="fa fa-pencil"></i></a>',
+        '&nbsp;&nbsp; <a href="#" class="remove-row" data-toggle="modal" data-target="#myModal"><i class="fa fa-trash-o"></i></a>'
+    ].join('')
+}
+
+function initTable(data, id) {
+    var myTableData;
+    myTableData = data;
+
+    var $table = $(id)
+    $table.bootstrapTable('destroy').bootstrapTable({
+        height: '100%',
+        resizable: true,
+        columns: [
+            {
+                valign: 'middle',
+                align: 'center',
+                width: 0,
+                formatter: function (value, row, index) {
+                    return index + 1;
+                }
+            },
+            {
+                field: 'code',
+                title: 'Code'
+
+            },
+            {
+                field: 'name',
+                title: 'Des'
+
+            },
+            {
+                field: 'name_cn',
+                title: 'Other Des'
+
+            },
+            {
+                field: 'price',
+                title: 'Price'
+
+            },
+            {
+                field: 'cost',
+                title: 'Cost'
+
+            },
+            {
+                field: 'id',
+                title: 'Action',
+                clickToSelect: false,
+                events: window.operateEvents,
+                formatter: operateFormatter
+            }
+        ],
+        data: myTableData
     });
 
-}).apply(this, [jQuery]);
+    $table.on('check.bs.table uncheck.bs.table ' +
+        'check-all.bs.table uncheck-all.bs.table',
+        function () {
+        $remove.prop('disabled', !$table.bootstrapTable('getSelections').length)
+        selections = getIdSelections()
+            // push or splice the selections if you want to save all data selections
+        })
+    $table.on('all.bs.table', function (e, name, args) {
+        console.log(name, args)
+    })
+
+    $(window).resize(function () {
+        $('#itemlist').bootstrapTable('resetView');
+    });
+}
+
+
+
