b43011ad20fea27e1bf6256d190a8d837fea59f0
eric2000.c <eric2000.c@gmail.com>
2019-10-03 10:53:27 +1300

    11

--- wwwroot/4/production/js/ItemEdit.js
+++ wwwroot/4/production/js/ItemEdit.js
@@ -24,64 +24,86 @@
     //priceVal = parseFloat(price.replace(/[^0-9-.]/g, '')); // 12345.99
 });
 
-$(document).ready(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    var actions = $("table td:last-child").html();
-    // Append table with add row form on add new button click
-    $(".add-new").click(function () {
-        $(this).attr("disabled", "disabled");
-        var index = $("table tbody tr:last-child").index();
-        var row = '<tr>' + '<td></td><td></td>'+
-            '<td><input type="text" class="form-control" name="code" id="code"></td>' +
-            '<td><input type="text" class="form-control" name="name" id="name"></td>' +
-            '<td><input type="text" class="form-control" name="name_cn" id="name_cn"></td>' +
-            '<td><input type="text" class="form-control" name="price" id="name_cn"></td>' +
-            '<td><input type="text" class="form-control" name="cost" id="name_cn"></td>' +
-            '<td><a href="#" class="on-editing save-row"><i class="fa fa-save"></i></a>&nbsp;&nbsp;<a href="#" class="on-default remove-row"><i class="fa fa-trash-o"></i></a></td>' +
-            '</tr>';
-        $("table").append(row);
-        $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-    // Add row on add button click
-    $(document).on("click", ".add", function () {
-        var empty = false;
-        var input = $(this).parents("tr").find('input[type="text"]');
-        input.each(function () {
-            if (!$(this).val()) {
-                $(this).addClass("error");
-                empty = true;
-            } else {
-                $(this).removeClass("error");
-            }
-        });
-        $(this).parents("tr").find(".error").first().focus();
-        if (!empty) {
-            input.each(function () {
-                $(this).parent("td").html($(this).val());
-            });
-            $(this).parents("tr").find(".add, .edit").toggle();
-            $(".add-new").removeAttr("disabled");
-        }
-    });
-    // Edit row on edit button click
-    $(document).on("click", ".edit", function () {
-        $(this).parents("tr").find("td:not(:last-child)").each(function () {
-            $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-        });
-        $(this).parents("tr").find(".add, .edit").toggle();
-        $(".add-new").attr("disabled", "disabled");
-    });
-    // Delete row on delete button click
-    $(document).on("click", ".delete", function () {
-        $(this).parents("tr").remove();
-        $(".add-new").removeAttr("disabled");
-    });
-});
+//$(document).ready(function () {
+//    $('[data-toggle="tooltip"]').tooltip();
+//    var actions = $("table td:last-child").html();
+//    // Append table with add row form on add new button click
+//    $(".add-new").click(function () {
+//        $(this).attr("disabled", "disabled");
+//        var index = $("table tbody tr:last-child").index();
+//        var row = '<tr>' + '<td></td><td></td>'+
+//            '<td><input type="text" class="form-control" name="code" id="code"></td>' +
+//            '<td><input type="text" class="form-control" name="name" id="name"></td>' +
+//            '<td><input type="text" class="form-control" name="name_cn" id="name_cn"></td>' +
+//            '<td><input type="text" class="form-control" name="price" id="name_cn"></td>' +
+//            '<td><input type="text" class="form-control" name="cost" id="name_cn"></td>' +
+//            '<td><a href="#" class="on-editing save-row"><i class="fa fa-save"></i></a>&nbsp;&nbsp;<a href="#" class="on-default remove-row"><i class="fa fa-trash-o"></i></a></td>' +
+//            '</tr>';
+//        $("table").append(row);
+//        $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+//        $('[data-toggle="tooltip"]').tooltip();
+//    });
+//    // Add row on add button click
+//    $(document).on("click", ".add", function () {
+//        var empty = false;
+//        var input = $(this).parents("tr").find('input[type="text"]');
+//        input.each(function () {
+//            if (!$(this).val()) {
+//                $(this).addClass("error");
+//                empty = true;
+//            } else {
+//                $(this).removeClass("error");
+//            }
+//        });
+//        $(this).parents("tr").find(".error").first().focus();
+//        if (!empty) {
+//            input.each(function () {
+//                $(this).parent("td").html($(this).val());
+//            });
+//            $(this).parents("tr").find(".add, .edit").toggle();
+//            $(".add-new").removeAttr("disabled");
+//        }
+//    });
+//    // Edit row on edit button click
+//    $(document).on("click", ".edit", function () {
+//        $(this).parents("tr").find("td:not(:last-child)").each(function () {
+//            $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+//        });
+//        $(this).parents("tr").find(".add, .edit").toggle();
+//        $(".add-new").attr("disabled", "disabled");
+//    });
+//    // Delete row on delete button click
+//    $(document).on("click", ".delete", function () {
+//        $(this).parents("tr").remove();
+//        $(".add-new").removeAttr("disabled");
+//    });
+//});
+
+
 
 
 var $table = $('#itemlist')
-var $remove = $('#remove')
+var $button = $('#addrow')
+//var $remove = $('#remove')
+
+$(function () {
+    $button.click(function () {
+        var randomId = 100 + ~~(Math.random() * 100)
+        $table.bootstrapTable('insertRow', {
+            index: 0,
+            row: {
+                id: randomId,
+                code: randomId,
+                name: 'Item ' + randomId,
+                name_cn: '新产品',
+                price: '$' + randomId,
+                cost: '$' + '0.00',
+                action: operateFormatter
+            }
+        })
+    })
+})
+
 function loaddata()
 {
     getdata();
